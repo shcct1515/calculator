d06b6dd46f810e96dd5782cb81aa8d6bf0c96424
shcct <shcct1254@gmail.com>
2025-01-26 19:34:17 +0700

    add first update

---
+++ calculator/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace calculator
+{
+    partial class calculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(112, 176);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 43);
+            button1.TabIndex = 0;
+            button1.Text = "equal";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(112, 96);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(585, 51);
+            textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(713, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 43);
+            button2.TabIndex = 2;
+            button2.Text = "Close";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(713, 405);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 33);
+            button3.TabIndex = 3;
+            button3.Text = "Ans";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // calculator
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            ControlBox = false;
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "calculator";
+            Text = "Form1";
+            Load += calculator_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ calculator/Form1.cs
@@ -0,0 +1,72 @@
+namespace calculator
+{
+    public partial class calculator : Form
+    {
+        string userinput;
+        string[] op;
+        double num1, num2;
+        double equal = 0;
+
+        public calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Exit?", "Close", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userinput = textBox1.Text;
+            op = userinput.Split(' ');
+            num1 = Convert.ToDouble(op[0]);
+            num2 = Convert.ToDouble(op[2]);
+            equal = dosomething();
+            textBox1.Text = Convert.ToString(equal);
+        }
+
+        private double dosomething()
+        {
+            switch (op[1])
+            {
+                case "+":
+                    return num1 + num2;
+                case "-":
+                    return num1 - num2;
+                case "*":
+                    return num1 * num2;
+                case "/":
+                    if (num2 == 0)
+                    {
+                        MessageBox.Show("khong the chia cho 0");
+                        return 0;
+                    }
+                    else
+                    {
+                        return Math.Round(num1 / num2, 3);
+                    }
+                default:
+                    break;
+            }
+            return 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double ans = equal;
+            textBox1.Text = Convert.ToString(ans);
+        }
+
+    }
+}
